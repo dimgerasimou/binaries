9a129b097e2a1955445ff96043bfa9a6915dfde8
Dimitris Gerasimou <gerasimou@ece.auth.gr>
2023-03-09 02:15:00 +0200

    Repo init

---
+++ dwmblocks/c-source/colorscheme.h
@@ -0,0 +1,9 @@
+#ifndef COLORSCHEME
+#define COLORSCHEME
+
+#define NBG "^b#323232^" /* normal background    */
+#define NFG "^c#BBBBBB^" /* normal foreground    */
+#define SBG "^b#296991^" /* selection background */
+#define SFG "^c#EEEEEE^" /* selection foreground */
+
+#endif  /* COLORSCHEME */
