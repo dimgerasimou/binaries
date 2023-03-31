2c45b07daa2e9d794f117c5623c611ee32f32747
Dimitris Gerasimou <gerasimou@ece.auth.gr>
2023-04-01 01:21:53 +0300

    date in c

---
+++ dwmblocks/c-source/date.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "colorscheme.h"
+
+const char *months[] = {"January", "February", "March", "April", "May", "June", "July",
+                        "August", "Semptember", "October", "November", "December"};
+
+const int daysinmonth[]={31,0,31,30,31,30,31,31,30,31,30,31};
+
+int firstdayinmonth(int mday, int wday) {
+	while(mday > 7) 
+		mday -= 7;
+
+	while(mday > 1) {
+		mday--;
+		wday--;
+		if(wday == -1)
+			wday = 6;
+	}
+
+	wday--;
+	if(wday == -1)
+		wday = 6;
+	return wday;
+}
+
+int calculatemonthdays(int month, int year) {
+	if (month != 1)
+		return daysinmonth[month];
+	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+		return 29;
+	return 28;
+}
+
+void getcalendar(char *calendar, int mday, int wday, int month, int year) {
+	int firstday = firstdayinmonth(mday, wday);
+	int monthdays = calculatemonthdays(month, year);
+	int daycounter;
+	char day[64];
+
+	strcpy(calendar, "Mo Tu We Th Fr <span color='#F38BA8'>Sa Su</span>\n");
+	for (int i = 0; i < firstday; i++) {
+		strcat(calendar, "   ");
+	}
+
+	for (int i = 1; i <= monthdays; i++) {
+		if (i == mday)
+			sprintf(day, "<span color='black' bgcolor='#F38BA8'>%2d</span> ", i);
+		else if (firstday == 5 || firstday == 6)
+			sprintf(day, "<span color='#F38BA8'>%2d</span> ", i);
+		else	
+			sprintf(day, "%2d ", i);
+
+		if (firstday == 7) {
+			firstday = 0;
+			strcat(calendar, "\n");
+		}
+		strcat(calendar, day);
+		firstday++;
+	}
+}
+
+void getheader(char *header, int month, int year) {
+	char yearstr[6];
+	char temp[64];
+	int padding;
+
+	strcpy(header, "");
+	strcat(temp, months[month]);
+	sprintf(yearstr, " %d", year);
+	strcat(temp, yearstr);
+
+	padding = (20 - strlen(temp)) / 2;
+	for(int i=0; i < padding; i++)
+		strcat(header, " ");
+	strcat(header, temp);
+}
+	
+
+int main() {
+	time_t currentTime = time(NULL);
+	struct tm* localTime = localtime(&currentTime);
+	char *env = getenv("BLOCK_BUTTON");
+	char date[64];
+	int pid;
+
+	if (env != NULL && strcmp(env, "1") == 0) {
+		pid = fork();
+		if (!pid) {
+			char calendar[256];
+			char header[64];
+	
+			getheader(header, localTime->tm_mon, localTime->tm_year + 1900);
+			getcalendar(calendar, localTime->tm_mday, localTime->tm_wday, localTime->tm_mon, localTime->tm_year + 1900);
+			execl("/bin/dunstify", "dunstify", header, calendar, NULL);
+			return 0;
+		}
+	}
+
+	printf(CLR_1" %02d/%02d"NRM"\n", localTime->tm_mday, localTime->tm_mon);
+	return 0;
+}
