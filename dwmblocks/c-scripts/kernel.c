3196b1a40757dbc9fba91f2dbb3fc69e704558fa
Dimitris Gerasimou <gerasimou@ece.auth.gr>
2023-04-03 07:32:01 +0300

    added packages to kernel

--- dwmblocks/c-scripts/kernel.c
+++ dwmblocks/c-scripts/kernel.c
@@ -1,17 +1,42 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/utsname.h>
+
 #include "colorscheme.h"
+
+int checkupdates(char *command) {
+	FILE *ep;
+	char buffer[64];
+	int counter = 0;
+	if ((ep = popen(command, "r")) == NULL)
+		return counter;
+	while(fgets(buffer, 64, ep) != NULL)
+		counter++;
+	pclose(ep);
+	return counter;
+}
+
+void updatecounter(int *aur, int *pacman) {
+	*aur = checkupdates("/bin/paru -Qua");
+	*pacman = checkupdates("/bin/checkupdates");
+}
 
 int main() {
 	struct utsname buffer;
 	char* release;
+	int aurupdates;
+	int pacmanupdates;
+
+	updatecounter(&aurupdates, &pacmanupdates);
 
 	if (uname(&buffer)) {
 		puts("Failed to allocate utsname struct");
 		return 1;
         }
 	release = strtok(buffer.release, "-");
+	if ((aurupdates + pacmanupdates) > 0)
+		printf(CLR_4"󰏖 %d "NRM, aurupdates + pacmanupdates);
 	printf(CLR_4" %s"NRM"\n", release);
         return 0;
 }
