1a75e2d2600b1a6817d2ec61bcf5fcc1a415d42e
Dimitris Gerasimou <gerasimou@ece.auth.gr>
2023-04-20 16:44:41 +0300

    Fixed bt scripts

--- dwmblocks/c-scripts/bluetooth-menu.c
+++ dwmblocks/c-scripts/bluetooth-menu.c
@@ -4,6 +4,11 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+struct device {
+	char mac[18];
+	char name[64];
+};
+
 int printmenu(char *menu, int menusize) {
 	int option = -1;
 	int writepipe[2], readpipe[2];
@@ -51,7 +56,6 @@ int getopstate() {
 		perror("Failed to execute bluetoothctl command");
 		exit(EXIT_FAILURE);
 	}
-
 	while (fgets(buf, 128, ep) != NULL) {
 		if (strstr(buf, "Powered") != NULL) {
 			pclose(ep);
@@ -67,8 +71,8 @@ int getopstate() {
 void togglebt() {
 	char cmd[64];
 	FILE *ep;
-	strcpy(cmd, "bluetoothctl power ");
 	
+	strcpy(cmd, "bluetoothctl power ");
 	if (getopstate())
 		strcat(cmd,"off");
 	else
@@ -77,83 +81,159 @@ void togglebt() {
 	pclose(ep);
 }
 
-void freearray(char **array, int size) {
-	for (int i = 0; i < size; i++) {
-		if (array[i] != NULL)
-			free(array[i]);
+void clearnline(char *string) {
+	for (int i = 0; i < strlen(string); i++) {
+		if (string[i] == '\n') {
+			string[i] = '\0';
+			return;
+		}
 	}
-	if (array != NULL)
-		free(array);
 }
 
-void connection(int state) {
-	char **info;	
-	FILE *ep;
-	char buffer[256];
-	int size = 0;
-	char *temp;
-	char cmd[64];
-	char *pipestring;
-	int opt;
+char *sttostr(struct device *list, int count) {
+	char *string = (char *) malloc(count * 64 * sizeof(char));
+	char temp[64];
 
-	if (state) {
-		strcpy(cmd, "bluetoothctl devices Connected");
-	} else {
-		strcpy(cmd, "bluetoothctl devices");
+	if (string == NULL)
+		return string;
+	string[0] = '\0';
+	for (int i = 0; i < count; i++) {
+		sprintf(temp, "%s\t%d\n", list[i].name, i);
+		strcat(string, temp);
 	}
-	
+	return string;
+}
+
+struct device *getcon(int *size, char *cmd) {
+	FILE *ep;
+	struct device *list;
+	int iter = 0;
+	char buf[256];
+	char *temp;
+
 	if ((ep = popen(cmd, "r")) == NULL) {
-		perror("Failed to execute bluetoothctl cmd");
+		perror("Failed in exec");
 		exit(EXIT_FAILURE);
 	}
-	if ((info = malloc(1 * sizeof(char*))) == NULL)
-		freearray(info, 1);
-	while(fgets(buffer, sizeof(buffer), ep) != NULL) {
-		size++;
-		info = realloc(info, size * sizeof(char*));
-		info[size - 1] = malloc(274 * sizeof(char));
-		if (info[size - 1] == NULL)
-			freearray(info, size);
-		temp = buffer;
-		temp += 7;
-		strcpy(info[size - 1], temp);
-		for (int i = strlen(info[size - 1]) - 1; i >= 0; i++) {
-			if (info[size - 1][i] == '\n') {
-				info[size - 1][i] = '\0';
-				break;
+
+	list = (struct device*) malloc(sizeof(struct device));
+	if (list == NULL) {
+		perror("Failed to allocate memory");
+		exit(EXIT_FAILURE);
+	}
+	while  (fgets(buf, sizeof(buf), ep) != NULL) {
+		if (strstr(buf, "Device") != NULL) {
+			iter++;
+			list = (struct device*) realloc(list, iter * sizeof(struct device*));
+			if (list == NULL) {
+				perror("Failed to allocate memory");
+				exit(EXIT_FAILURE);
 			}
+			temp = buf;
+			temp += 7;
+			strncpy(list[iter-1].mac, temp, 17);
+			temp += 17;
+			strcpy(list[iter-1].name, temp);
+			clearnline(list[iter-1].mac);
+			clearnline(list[iter-1].name);
 		}
 	}
 	pclose(ep);
+	*size = iter;
+	return list;
+}
 
-	if (!size) {
-		execl("/bin/dunstify", "dunstify", "          Bluetooth", "No available Bluetooth connections.", NULL);
+void menuandexec(struct device *list, int count, char *cmdin) {
+	FILE *ep;
+	char *pipestr;
+	char cmd[64];
+	int option;
+	
+	if((pipestr = sttostr(list, count)) == NULL) {
+		perror("Failed in allocating memory");
 		return;
 	}
-	pipestring = (char*) malloc(size * 160 * sizeof(char));
-	pipestring[0] = '\0';
+	option = printmenu(pipestr, strlen(pipestr));
+	if (pipestr != NULL)
+		free(pipestr);
+	
+	strcpy(cmd, cmdin);
+	strcat(cmd, list[option].mac);
+	ep = popen(cmd, "r");
+	pclose(ep);
+}
 
-	for (int i = 0; i < size; i++) {
-		temp = info[i];
-		temp += 18;
-		sprintf(buffer, "%s\t%d\n", temp, i);
-		strcat(pipestring, buffer);
+struct device *getinact(int *inasize, int actsize, int allsize, struct device *actlist, struct device *alllist) {
+	int iter = 0;
+	int maxsize = allsize - actsize;
+	int found;
+	
+	struct device *list = (struct device*) malloc(maxsize * sizeof(struct device));
+	if (list == NULL)
+		return list;
+	
+	for (int i = 0; i < allsize; i++) {
+		if (iter == maxsize)
+			break;
+		found = 0;
+		for (int j = 0; j < actsize; j++) {
+			if (strcmp(alllist[i].mac, actlist[i].mac) == 0)
+				found = 1;
+		}
+		if (!found) {
+			strcpy(list[iter].mac, alllist[i].mac);
+			strcpy(list[iter].name, alllist[i].name);
+			iter++;
+		}
 	}
+	if (iter < maxsize) {
+		list = (struct device*) realloc(list, iter * sizeof(struct device));
+	}
+	*inasize = iter;
+	return list;
+}
 
-	opt = printmenu(pipestring, strlen(pipestring));
-	if (opt >= 0) {
-		strncpy(buffer, info[opt], 17);
-		buffer[17] = '\0';
-		if (state) 
-			strcpy(cmd, "bluetoothctl disconnect ");
-		else 
-			strcpy(cmd, "bluetoothctl connect ");
-		strcat(cmd, buffer);
-		ep = popen(cmd, "r");
-		pclose(ep);
+void connect() {
+	int actsize;
+	int allsize;
+	int inasize;
+	struct device *actlist = getcon(&actsize, "bluetoothctl devices Connected");
+	struct device *alllist = getcon(&allsize, "bluetoothctl devices");
+	struct device *inalist;
+
+	if (!allsize) {
+		execl("/bin/dunstify", "dunstify", "Bluetooth", "No devices available.", NULL);
+		if (actlist != NULL)
+			free(actlist);
+		if (alllist != NULL)
+			free(alllist);
+		return;
 	}
-	if (pipestring != NULL)	free(pipestring);
-	freearray(info, size);	
+	inalist = getinact(&inasize, actsize, allsize, actlist, alllist);
+	if (actlist != NULL)
+		free(actlist);
+	if (alllist != NULL)
+		free(alllist);
+	if (inalist == NULL)
+		return;
+	menuandexec(inalist, inasize, "bluetoothctl connect ");
+	if (inalist != NULL)
+		free(inalist);
+}
+
+void disconnect() {
+	int size;
+	struct device *list = getcon(&size, "bluetoothctl devices Connected");
+	
+	if (!size) {
+		execl("/bin/dunstify", "dunstify", "Bluetooth", "No devices available.", NULL);
+		if (list != NULL)
+			free(list);
+		return;
+	}
+	menuandexec(list, size, "bluetoothctl disconnect ");	
+	if (list != NULL)
+		free(list);
 }
 
 int main(void) {
@@ -164,15 +244,13 @@ int main(void) {
 			togglebt();
 			break;
 		case 1:
-			connection(0);
+			connect();	
 			break;
 		case 2:
-			connection(1);
+			disconnect();
 			break;
 		default:
 			break;
 	}
 	return 0;
 }
-
-
--- dwmblocks/c-scripts/bluetooth.c
+++ dwmblocks/c-scripts/bluetooth.c
@@ -4,6 +4,11 @@
 #include <unistd.h>
 #include "colorscheme.h"
 
+struct device {
+	char mac[18];
+	char name[64];
+};
+
 int getopstate() {
 	FILE *ep;
 	char buf[128];
@@ -12,7 +17,6 @@ int getopstate() {
 		perror("Failed to execute bluetoothctl command");
 		exit(EXIT_FAILURE);
 	}
-
 	while (fgets(buf, 128, ep) != NULL) {
 		if (strstr(buf, "Powered") != NULL) {
 			pclose(ep);
@@ -23,6 +27,91 @@ int getopstate() {
 	}
 	pclose(ep);
 	return 0;
+}
+
+void clearnline(char *string) {
+	for (int i = 0; i < strlen(string); i++) {
+		if (string[i] == '\n') {
+			string[i] = '\0';
+			return;
+		}
+	}
+}
+
+struct device *getcon(int *size) {
+	FILE *ep;
+	struct device *list;
+	int iter = 0;
+	char buf[256];
+	char *temp;
+
+	if ((ep = popen("bluetoothctl devices Connected", "r")) == NULL) {
+		perror("Failed in exec");
+		exit(EXIT_FAILURE);
+	}
+
+	list = (struct device*) malloc(sizeof(struct device));
+	if (list == NULL) {
+		perror("Failed to allocate memory");
+		exit(EXIT_FAILURE);
+	}
+	while  (fgets(buf, sizeof(buf), ep) != NULL) {
+		if (strstr(buf, "Device") != NULL) {
+			iter++;
+			list = (struct device*) realloc(list, iter * sizeof(struct device*));
+			if (list == NULL) {
+				perror("Failed to allocate memory");
+				exit(EXIT_FAILURE);
+			}
+			temp = buf;
+			temp += 7;
+			strncpy(list[iter-1].mac, temp, 17);
+			temp += 17;
+			strcpy(list[iter-1].name, temp);
+			clearnline(list[iter-1].mac);
+			clearnline(list[iter-1].name);
+		}
+	}
+	pclose(ep);
+	*size = iter;
+	return list;
+}
+
+int getaudio(char *address) {
+	FILE *ep;
+	char cmd[128];
+	char buf[128];
+
+	strcpy(cmd, "bluetoothctl info ");
+	strcat(cmd, address);
+	if ((ep = popen(cmd, "r")) == NULL) {
+		perror("Failed to exec bluetoothctl");
+		return 0;
+	}
+	while(fgets(buf, sizeof(buf), ep) != NULL) {
+		if (strstr(buf, "Audio Sink") != NULL) {
+			pclose(ep);
+			return 1;
+		}
+	}
+	pclose(ep);
+	return 0;
+}
+
+int audiodevice() {
+	int size;
+	int isaudio = 0;
+	struct device *list = getcon(&size);
+	
+	for (int i = 0; i < size; i++) {
+		printf("Mac:%s, Name:%s\n", list[i].mac, list[i].name);
+		if (getaudio(list[i].mac))
+			isaudio = 1;
+	}
+	
+	if (list != NULL)
+		free(list);
+	return isaudio;
 }
 
 void execst() {
@@ -84,7 +173,10 @@ int main(void) {
 	if (!getopstate()) {
 		printf(CLR_4 "󰂲" NRM "\n");
 	} else {
-		printf(CLR_4 "󰂯" NRM "\n");
+		if (!audiodevice())
+			printf(CLR_4 "󰂯" NRM "\n");
+		else
+			printf(CLR_4 "󰥰" NRM "\n");
 	}
 	return EXIT_SUCCESS;
 }
