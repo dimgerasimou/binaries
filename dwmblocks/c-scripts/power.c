b85d148c4df390c02e77662c19c7b1459b8d0adc
Dimitris Gerasimou <dimgerasimou@gmail.com>
2023-07-19 04:07:07 +0300

    Fixed menu

--- dwmblocks/c-scripts/power.c
+++ dwmblocks/c-scripts/power.c
@@ -65,7 +65,7 @@ int isnumber(char* string) {
 
 void killproccess(char *name) {
 	FILE *fp;
-	struct dirent** pidlist;
+	struct dirent **pidlist;
 	int funcret = scandir("/proc", &pidlist, NULL, alphasort);
 	char buffer[128];
 	char filename[128];
@@ -103,38 +103,111 @@ void killproccess(char *name) {
 void restartdwmblocks() {
 	killproccess("dwmblocks");
 	switch (fork()) {
-		case -1:perror("Failed in forking");
+		case -1:
+			perror("Failed in forking");
 			exit(EXIT_FAILURE);
-
-		case 0:	setsid();
+		case 0:
+			setsid();
 			unsetenv("BLOCK_BUTTON");
 			execl("/usr/local/bin/dwmblocks", "dwmblocks", NULL);
 			exit(EXIT_SUCCESS);
-
 		default:
+			break;
 	}
 }
 
 void lock() {
 	switch (fork()) {
-		case -1:perror("Failed in forking");
+		case -1:
+			perror("Failed in forking");
 			exit(EXIT_FAILURE);
-
-		case 0:	setsid();
+		case 0:
+			setsid();
 			sleep(1);
 			execl("/usr/local/bin/slock", "slock", NULL);
 			exit(EXIT_SUCCESS);
-
 		default:
+			break;
+	}
+}
+
+void clippause() {
+	switch (fork()) {
+		case -1:
+			perror("Failed in forking");
+			exit(EXIT_FAILURE);
+		case 0:
+			setsid();
+			execl("/usr/bin/clipctl", "clipctl", "disable", NULL);
+			sleep(60);
+			execl("/usr/bin/clipctl", "clipctl", "enable", NULL);
+			exit(EXIT_SUCCESS);
+		default:
+			break;
+	}
+}
+
+void emptydir(char *path) {
+	struct dirent **dir;
+	int ret = scandir(path, &dir, NULL, NULL);
+	char filepath[256];
+
+	if (ret == -1) {
+		perror("Failed to scan the clipmenu directory");
+		freestruct(dir, ret);
+		exit(EXIT_FAILURE);
+	}
+
+	for (int i = 0; i < ret; i++) {
+		strcpy(filepath, path);
+		strcat(filepath, "/");
+		strcat(filepath, dir[i]->d_name);
+		remove(filepath);
+	}
+	freestruct(dir, ret);
+}
+
+void deleteclipall() {
+	switch (fork()) {
+		case -1:
+			perror("Failed in forking");
+			exit(EXIT_FAILURE);
+			break;
+		case 0:
+			FILE *ep;
+			char path[1024];
+			char *ptr = NULL;
+
+			if ((ep = popen("clipctl cache-dir", "r")) == NULL){
+				perror("Failed to exec clipctl cmd");
+				exit(EXIT_FAILURE);
+			}
+			fgets(path, 1024, ep);
+			pclose(ep);
+			if ((ptr = strchr(path, '\n')))
+				*ptr = '\0';
+			emptydir(path);
+			strcat(path, "/status");
+			ep = fopen(path, "w");
+			fprintf(ep, "enabled\n");
+			fclose(ep);
+			exit(EXIT_SUCCESS);
+		default:
+			break;
 	}
 }
 
 void executebutton() {
 	char *env = getenv("BLOCK_BUTTON");
-	char powermenu[] = " Shutdown\t0\n Reboot\t1\n Restart DwmBlocks\t2\n󰗽 Logout\t3\n Lock\t4";
-	int pmsz=sizeof(powermenu);
+	char powermenu[] = " Shutdown\t0\n Reboot\t1\n\n󰗽 Logout\t2\n Lock\t3\n\n Restart DwmBlocks\t4\n󰘚 Optimus Manager\t5\n󰅌 Clipmenu\t6";
 	char yesnoprompt[] = "Yes\t1\nNo\t0\n";
-	int ynsz=sizeof(yesnoprompt);
+	char optimusmenu[] = "Integrated\t0\nHybrid\t1\nNvidia\t2";
+	char clipmenu[] = "Pause clipmenu for 1 minute\t0\nClear clipboard\t1";
+	int pmsz = sizeof(powermenu);
+	int ynsz = sizeof(yesnoprompt);
+	int optimussz = sizeof(optimusmenu);
+	int clipsz = sizeof(clipmenu);
+
 	if (env == NULL || env[0] != '1')
 		return;
 
@@ -143,28 +216,53 @@ void executebutton() {
 			if (printmenu(yesnoprompt, ynsz) == 1)
 				execl("/bin/shutdown", "shutdown", "now", NULL);
 			break;
-
 		case 1:
 			if (printmenu(yesnoprompt, ynsz) == 1)
 				execl("/bin/shutdown", "shutdown", "now", "-r", NULL);
 			break;
-
 		case 2:
-			restartdwmblocks();
-			break;
-
-		case 3:
 			killproccess("/usr/local/bin/dwm");
 			break;
-
-		case 4:
+		case 3:
 			lock();
 			break;
+		case 4:
+			restartdwmblocks();
+			break;
+		case 5:
+			switch (printmenu(optimusmenu, optimussz)) {
+				case 0:
+					if (printmenu(yesnoprompt, ynsz) == 1)
+						execl("/bin/optimus-manager", "--no-confirm", "--switch", "integrated", NULL);
+					break;
+				case 1:
+					if (printmenu(yesnoprompt, ynsz) == 1)
+						execl("/bin/optimus-manager", "--no-confirm", "--switch", "hybrid", NULL);
+					break;
+				case 2:
+					if (printmenu(yesnoprompt, ynsz) == 1)
+						execl("/bin/optimus-manager", "--no-confirm", "--switch", "nvidia", NULL);
+					break;
+				default:
+					break;
+			}
 
+		case 6:
+			switch (printmenu(clipmenu, clipsz)) {
+				case 0:
+					clippause();
+					break;
+				case 1:
+					deleteclipall();
+					break;
+				default:
+					break;
+			}
 		default:
 			break;
 	}
 }
+
 
 int main(void) {
 	executebutton();
