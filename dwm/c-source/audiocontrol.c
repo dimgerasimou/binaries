616730fc18c13a56a00593ae168a1c1ba18c257f
Dimitris Gerasimou <dimgerasimou@gmail.com>
2023-07-19 19:34:02 +0300

    Using dwmblocksctl

--- dwm/c-source/audiocontrol.c
+++ dwm/c-source/audiocontrol.c
@@ -7,24 +7,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <dirent.h>
-#include <signal.h>
 
 #define SIGNALNO 10    /* signal number of dwmblocks volume block */
 #define MAXVOL "1.2"   /* max volume to raise the sink 1.2 = 120% */
-
-void freestruct(struct dirent** input, int n) {
-	while (n--)
-		free(input[n]);
-	free(input);
-}
-
-int isnumber(char* string) {
-	for (int i = 0; i < strlen(string); i++)
-		if (string[i] > '9' || string[i] < '0')
-			return 0;
-	return 1;
-}
 
 int main(int argc, char *argv[]) {
 	// Return if 2 arguments are not given.
@@ -65,43 +50,10 @@ int main(int argc, char *argv[]) {
 	else
 		strcat(output, "toggle");
 
-	FILE* fp;
-	fp = popen(output, "r");
-	pclose(fp);
-	struct dirent** pidlist;
-
-	int funcret = scandir("/proc", &pidlist, NULL, alphasort); 
-	if (funcret == -1) {
-		perror("dwm-audiocontrol: Failed to scand /proc directory");
-		freestruct(pidlist, funcret);
-		exit(EXIT_FAILURE);
-	}
-	
-	char buffer[128];
-	char filename[128];
-	int pid = -1;
-
-	for (int i = 0; i < funcret; i++) {
-		if (isnumber(pidlist[i]->d_name)) {
-			strcpy(filename, "/proc/");
-			strcat(filename, pidlist[i]->d_name);
-			strcat(filename, "/cmdline");
-			fp = fopen(filename, "r");
-			if (fp == NULL) {
-				fclose(fp);
-				continue;
-			}
-			fgets(buffer, sizeof(buffer), fp);
-			fclose(fp);
-			if (strstr(buffer, "dwmblocks") != NULL) { 
-				pid = strtol(pidlist[i]->d_name, NULL, 10);
-				break;
-			}
-		}
-	}
-
-	kill(pid, SIGRTMIN + SIGNALNO);
-	freestruct(pidlist, funcret);
-	return 0;
+	FILE* ep;
+	ep = popen(output, "w");
+	pclose(ep);
+	ep = popen("/usr/local/bin/dwmblocksctl -s volume", "w");
+	pclose(ep);
+	return 0;		
 }
-
