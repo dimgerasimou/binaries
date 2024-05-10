b33de1b6600ac9c0757a806840486ad692a5956b
Dimitris <dimgerasimou@gmail.com>
2024-05-10 22:19:21 +0300

    Unspaghetti battery.c and add log functionality

--- dwmblocks/status-bar-exec/src/battery.c
+++ dwmblocks/status-bar-exec/src/battery.c
@@ -8,115 +8,104 @@
 
 #define OPTIMUS
 
-char *baticons[] = { CLR_1" "NRM, CLR_3" "NRM, CLR_2" "NRM, CLR_2" "NRM, CLR_2" "NRM };
+const char *pmcmd      = "optimus-manager --status";
+const char *cappath    = "/sys/class/power_supply/BAT1/capacity";
+const char *statuspath = "/sys/class/power_supply/BAT1/status";
+const char *baticons[] = { CLR_1" "NRM, CLR_3" "NRM, CLR_2" "NRM, CLR_2" "NRM, CLR_2" "NRM };
+const char *models[]   = { "Unmanaged", "Integrated", "Hybrid", "Nvidia"};
+const char *iconls[]   = { NULL, "intel", "deepin-graphics-driver-manager", "nvidia"};
 
-#if defined OPTIMUS
-void getmode(char *mode, char *icon) {
+int
+getmode()
+{
 	FILE *ep;
 	char buffer[64];
+	int  ret = 0;
 
-	if ((ep = popen("optimus-manager --status", "r")) == NULL) {
-		perror("Failed to execute optimus-manager");
+	if (!(ep = popen(pmcmd, "r"))) {
+		char log[512] = "Failed to execute ";
+
+		strcat(log, pmcmd);
+		log_string(log, "dwmblocks-battery");
 		exit(EXIT_FAILURE);
 	}
 
-	while(fgets(buffer, 64, ep) != NULL)
-		if (strstr(buffer, "Current") != NULL)
+	while(fgets(buffer, 64, ep))
+		if (strstr(buffer, "Current"))
 			break;
 
-	if (strstr(buffer, "integrated") != NULL) {
-		strcpy(mode, "Optimus Manager: Integrated");
-		strcpy(icon, "intel");
-	} else if (strstr(buffer, "hybrid") != NULL) {
-		strcpy(mode, "Optimus Manager: Hybrid");
-		strcpy(icon, "deepin-graphics-driver-manager");
-	} else if (strstr(buffer, "nvidia") != NULL) {
-		strcpy(mode, "Optimus Manager: Nvidia");
-		strcpy(icon, "nvidia");
-	} else {
-		strcpy(mode, "Optimus Manager: Unmanaged");
+	if (strstr(buffer, "integrated")) {
+		ret = 1;
+	} else if (strstr(buffer, "hybrid")) {
+		ret = 2;
+	} else if (strstr(buffer, "nvidia")) {
+		ret = 3;
 	}
+
 	pclose(ep);
+	return ret;
 }
-#elif defined ENVY
-void getmode(char *mode, char *icon) {
-	FILE *ep;
-	char buffer[64];
 
-	if ((ep = popen("envycontrol -q", "r")) == NULL) {
-		perror("Failed to execute optimus-manager");
-		exit(EXIT_FAILURE);
-	}
-
-	fgets(buffer, 64, ep);
-	pclose(ep);
-
-	if (strstr(buffer, "integrated") != NULL) {
-		strcpy(mode, "Envy Control: Integrated");
-		strcpy(icon, "intel");
-	} else if (strstr(buffer, "hybrid") != NULL) {
-		strcpy(mode, "Envy Control: Hybrid");
-		strcpy(icon, "deepin-graphics-driver-manager");
-	} else if (strstr(buffer, "nvidia") != NULL) {
-		strcpy(mode, "Envy Control: Nvidia");
-		strcpy(icon, "nvidia");
-	} else {
-		strcpy(mode, "Envy Control: Unmanaged");
-	}
-}
-#else
-void getmode(char *mode, char *icon) {
-	strcpy(mode, "Optimus: Not managed");
-}
-#endif
-
-void modenotify(int capacity, char *status) {
+void
+modenotify(int capacity, char *status)
+{
 	char body[256];
-	char icon[64];
-	char mode[64]; 
+	int mode = getmode();
 	
-	strcpy(icon, "");
-	sprintf(body, "Battery capacity: %d%%\nBattery status: %s\n", capacity, status);
-
-	getmode(mode, icon);
-	strcat(body, mode);
-	notify("Power", body, icon, NOTIFY_URGENCY_LOW, 0);
+	sprintf(body, "Battery capacity: %d%%\nBattery status: %s\nOptimus manager: ", capacity, status);
+	strcat(body, models[mode]);
+	notify("Power", body, (char*) iconls[mode], NOTIFY_URGENCY_LOW, 0);
 }
 
-void executebutton(int capacity, char *status) {
+void
+executebutton(int capacity, char *status)
+{
 	char *env = getenv("BLOCK_BUTTON");
 
-	if (env != NULL && strcmp(env, "1") == 0)
+	if (env && strcmp(env, "1") == 0)
 		modenotify(capacity, status);
 }
 
-int main(void) {
+int
+main(void)
+{
 	FILE *fp;
-	int capacity;
+	int  capacity;
 	char status[64];
 
-	if ((fp = fopen("/sys/class/power_supply/BAT1/capacity", "r")) == NULL) {
-		perror("Failed to read \"/sys/class/power_supply/BAT1/capacity\"");
-		return EXIT_FAILURE;
+	if (!(fp = fopen(cappath, "r"))) {
+		char log[512] = "Failed to open ";
+
+		strcat(log, cappath);
+		log_string(log, "dwmblocks-battery");
+		exit(EXIT_FAILURE);
         }
+
        	fscanf(fp, "%d", &capacity);
         fclose(fp);
 
-	if ((fp = fopen("/sys/class/power_supply/BAT1/status", "r")) == NULL) {
-		perror("Failed to read \"/sys/class/power_supply/BAT1/status\"");
-		return EXIT_FAILURE;
+	if (!(fp = fopen(statuspath, "r"))) {
+		char log[512] = "Failed to open ";
+
+		strcat(log, status);
+		log_string(log, "dwmblocks-battery");
+		exit(EXIT_FAILURE);
         }
+
         fgets(status, 64, fp);
 	fclose(fp);
-	for (int i = 0; i < (int) strlen(status); i++) {
-		if (status[i] == '\n')
-			status[i] = '\0';
+
+	for (char *ptr = status; *status != '\0'; ptr++) {
+		if (*ptr =='\n') {
+			*ptr = '\0';
+			break;
+		}
 	}
 		
 	executebutton(capacity, status);
 
 	if(strcmp(status, "Charging") == 0) {
-		printf(CLR_3 BG_1" "NRM"\n");
+		printf(CLR_3 BG_1"  "NRM"\n");
 		return EXIT_SUCCESS;
 	}
 	
