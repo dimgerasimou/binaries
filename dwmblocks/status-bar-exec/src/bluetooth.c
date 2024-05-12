a52141897e660bbb5c2eb1d1d7f8a978ff00b749
Dimitris <dimgerasimou@gmail.com>
2024-05-12 05:13:20 +0300

    Unspaghetti bluetooth.c

--- dwmblocks/status-bar-exec/src/bluetooth.c
+++ dwmblocks/status-bar-exec/src/bluetooth.c
@@ -5,152 +5,149 @@
 #include "../include/colorscheme.h"
 #include "../include/common.h"
 
-const char *termpath   = "/usr/local/bin/st";
-const char *termargs[] = {"st", "-e", "bluetuith", NULL};
+const char *tui_path[]  = { "usr", "local", "bin", "st", NULL};
+const char *tui_args[]  = {"st", "-e", "bluetuith", NULL};
+const char *menu_path[] = {"$HOME", ".local", "bin", "dwmblocks", "bluetooth-menu"};
+const char *menu_args[] = {"bluetooth-menu", NULL};
+const char *icon_ls[]   = {" 󰂲 ", " 󰂯 ", " 󰥰 "};
+const char *bt_show     = "bluetoothctl show";
+const char *bt_info     = "bluetoothctl info";
+const char *bt_con      = "bluetoothctl devices Connected";
 
-struct device {
-	char mac[18];
-	char name[64];
-};
-
-int getopstate() {
+int
+is_powered()
+{
 	FILE *ep;
-	char buf[128];
-	
-	if ((ep = popen("bluetoothctl show", "r")) == NULL) {
-		perror("Failed to execute bluetoothctl command");
+	char buffer[128];
+
+	if (!(ep = popen(bt_show, "r"))) {
+		char log[512] = "Failed to execute ";
+
+		strcat(log, bt_show);
+		log_string(log, "dwmblocks-bluetooth");
 		exit(EXIT_FAILURE);
 	}
-	while (fgets(buf, 128, ep) != NULL) {
-		if (strstr(buf, "Powered") != NULL) {
+
+	while (fgets(buffer, 128, ep)) {
+		if (strstr(buffer, "Powered")) {
 			pclose(ep);
-		if (strstr(buf, "yes") != NULL)
-			return 1;
-		return 0;
+
+			if (strstr(buffer, "yes"))
+				return 1;
+
+			return 0;
 		}
 	}
+
 	pclose(ep);
 	return 0;
 }
 
-void clearnline(char *string) {
-	for (int i = 0; i < (int) strlen(string); i++) {
-		if (string[i] == '\n') {
-			string[i] = '\0';
-			return;
-		}
-	}
-}
-
-struct device *getcon(int *size) {
-	FILE *ep;
-	struct device *list;
-	int iter = 0;
-	char buf[256];
-	char *temp;
-
-	if ((ep = popen("bluetoothctl devices Connected", "r")) == NULL) {
-		perror("Failed in exec");
-		exit(EXIT_FAILURE);
-	}
-
-	list = (struct device*) malloc(sizeof(struct device));
-	if (list == NULL) {
-		perror("Failed to allocate memory");
-		exit(EXIT_FAILURE);
-	}
-	while  (fgets(buf, sizeof(buf), ep) != NULL) {
-		if (strstr(buf, "Device") != NULL) {
-			iter++;
-			list = (struct device*) realloc(list, iter * sizeof(struct device*));
-			if (list == NULL) {
-				perror("Failed to allocate memory");
-				exit(EXIT_FAILURE);
-			}
-			temp = buf;
-			temp += 7;
-			strncpy(list[iter-1].mac, temp, 17);
-			temp += 17;
-			strcpy(list[iter-1].name, temp);
-			clearnline(list[iter-1].mac);
-			clearnline(list[iter-1].name);
-		}
-	}
-	pclose(ep);
-	*size = iter;
-	return list;
-}
-
-int getaudio(char *address) {
+int
+audio_device(char *address)
+{
 	FILE *ep;
 	char cmd[128];
 	char buf[128];
 
-	strcpy(cmd, "bluetoothctl info ");
+	strcpy(cmd, bt_info);
+	strcat(cmd, " ");
 	strcat(cmd, address);
-	if ((ep = popen(cmd, "r")) == NULL) {
-		perror("Failed to exec bluetoothctl");
-		return 0;
+
+	if (!(ep = popen(cmd, "r"))) {
+		char log[512] = "Failed to execute ";
+
+		strcat(log, cmd);
+		log_string(log, "dwmblocks-bluetooth");
+		exit(EXIT_FAILURE);
 	}
-	while(fgets(buf, sizeof(buf), ep) != NULL) {
-		if (strstr(buf, "audio-headset") != NULL) {
+
+	while(fgets(buf, sizeof(buf), ep)) {
+		if (strstr(buf, "audio-headset")) {
 			pclose(ep);
 			return 1;
 		}
 	}
+
 	pclose(ep);
 	return 0;
 }
 
-int audiodevice() {
-	int size;
-	int isaudio = 0;
-	struct device *list = getcon(&size);
-	
-	for (int i = 0; i < size; i++) {
-		if (getaudio(list[i].mac))
-			isaudio = 1;
+int
+has_audio()
+{
+	FILE *ep;
+	char buffer[256];
+	char mac[18];
+	char *temp;
+
+	if (!(ep = popen(bt_con, "r"))) {
+		char log[512] = "Failed to execute ";
+
+		strcat(log, bt_show);
+		log_string(log, "dwmblocks-bluetooth");
+		exit(EXIT_FAILURE);
 	}
-	
-	if (list != NULL)
-		free(list);
-	return isaudio;
+
+	while (fgets(buffer, sizeof(buffer), ep)) {
+		if (strstr(buffer, "Device")) {
+			temp = buffer;
+			temp += 7;
+
+			strncpy(mac, temp, 17);
+			sanitate_newline(temp);
+
+			if (audio_device(mac)) {
+				pclose(ep);
+				return 1;
+			}
+		}
+	}
+
+	pclose(ep);
+	return 0;
 }
 
-void execblock() {
+void
+execute_block()
+{
 	char *button;
-	char path[256];
-	char *env;
-	const char *args[] = {"bluetooth-menu", NULL};
-	
+	char *path = NULL;
+
 	if ((button = getenv("BLOCK_BUTTON")) == NULL)
 		return;
 
 	switch (button[0] - '0') {
 		case 1:
-			env = getenv("HOME");
-			strcpy(path, env);
-			strcat(path, "/.local/bin/dwmblocks/bluetooth-menu");
-			forkexecv(path, (char**) args);
+			path = get_path((char**) menu_path, 1);
+			forkexecv(path, (char**) menu_args);
 			break;
+
 		case 2:
-			forkexecv((char*) termpath, (char**) termargs);
+			path = get_path((char**) tui_path, 1);
+			forkexecv(path, (char**) tui_args);
 			break;
+
 		default:
 			break;
 	}
+
+	if (path)
+		free(path);
 }
 
-int main(void) {
-	execblock();
+int
+main(void)
+{
+	int icon_arg = 0;
 
-	if (!getopstate()) {
-		printf(CLR_4 BG_1 " 󰂲" NRM "\n");
-	} else {
-		if (!audiodevice())
-			printf(CLR_4 BG_1" 󰂯" NRM "\n");
-		else
-			printf(CLR_4 BG_1" 󰥰" NRM "\n");
-	}
+	execute_block();
+
+	icon_arg += is_powered();
+	if (icon_arg)
+		icon_arg += has_audio();
+
+	printf(CLR_4 BG_1"%s" NRM "\n", icon_ls[icon_arg]);
+
 	return EXIT_SUCCESS;
 }
