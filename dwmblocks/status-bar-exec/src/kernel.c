e30f0bd8d06d2f6b6f389913515a18739c71474a
Dimitris <dimgerasimou@gmail.com>
2024-05-14 21:24:46 +0300

    Unspaghetti kernel.c

--- dwmblocks/status-bar-exec/src/kernel.c
+++ dwmblocks/status-bar-exec/src/kernel.c
@@ -9,26 +9,38 @@
 #include "../include/colorscheme.h"
 #include "../include/common.h"
 
-char *updatecommand[] = {"st", "-e", "sh", "-c", "paru", NULL};
+const char *updatecommand[] = {"st", "-e", "sh", "-c", "paru", NULL};
+const char *aurupdatescmd   = "/bin/paru -Qua";
+const char *pmupdatescmd    = "/bin/checkupdates";
 
-int checkupdates(char *command) {
+
+int
+checkupdates(const char *command)
+{
 	FILE *ep;
 	char buffer[64];
-	int counter = 0;
+	int  counter = 0;
+
 	if ((ep = popen(command, "r")) == NULL)
 		return counter;
+
 	while(fgets(buffer, 64, ep) != NULL)
 		counter++;
+
 	pclose(ep);
 	return counter;
 }
 
-void updatecounter(int *aur, int *pacman) {
-	*aur = checkupdates("/bin/paru -Qua");
-	*pacman = checkupdates("/bin/checkupdates");
+void
+updatecounter(int *aur, int *pacman)
+{
+	*aur    = checkupdates(aurupdatescmd);
+	*pacman = checkupdates(pmupdatescmd);
 }
 
-void executebutton(int aur, int pacman) {
+void
+executebutton(int aur, int pacman)
+{
 	char *env = getenv("BLOCK_BUTTON");
 
 	if (env == NULL)
@@ -41,23 +53,27 @@ void executebutton(int aur, int pacman) {
 	}
 }
 
-int main() {
+int
+main(void)
+{
 	struct utsname buffer;
-	char* release;
-	int aurupdates;
-	int pacmanupdates;
+	char*          release;
+	int            aurupdates;
+	int            pacmanupdates;
 
 	updatecounter(&aurupdates, &pacmanupdates);
 	executebutton(aurupdates, pacmanupdates);
 
 	if (uname(&buffer)) {
-		puts("Failed to allocate utsname struct");
+		log_string("Failed to allocate utsname struct", "dwmblocks-kernel");
 		return 1;
-        }
+	}
+
 	release = strtok(buffer.release, "-");
 	if ((aurupdates + pacmanupdates) > 0)
 		printf(CLR_12"  󰏖 %d  %s"NRM"\n", aurupdates + pacmanupdates, release);
 	else
 		printf(CLR_12"   %s"NRM"\n", release);
-        return 0;
+
+	return 0;
 }
