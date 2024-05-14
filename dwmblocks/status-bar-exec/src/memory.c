8f53bea38b410e4c3d3b3d449fbde21549d40621
Dimitris <dimgerasimou@gmail.com>
2024-05-14 22:00:10 +0300

    Unspaghetti memory.c

--- dwmblocks/status-bar-exec/src/memory.c
+++ dwmblocks/status-bar-exec/src/memory.c
@@ -6,37 +6,42 @@
 #include "../include/colorscheme.h"
 #include "../include/common.h"
 
-const char *htoppath = "/usr/local/bin/st";
+const char *htoppath   = "/usr/local/bin/st";
 const char *htopargs[] = {"st", "-e", "sh", "-c", "htop", NULL};
 
-long calculateused() {
+long
+calculateused()
+{
 	FILE *fp;
 	char buffer[128];
 	char *ptr;
 	long used=0, temp;
 
-	if ((fp = fopen("/proc/meminfo", "r")) == NULL)
+	if (!(fp = fopen("/proc/meminfo", "r")))
 		return 0;
 
-	while (fgets(buffer, 128, fp) != NULL) {
+	while (fgets(buffer, 128, fp)) {
 		if (strstr(buffer, "MemTotal") != 0) {
 			ptr = (strchr(buffer, ':')) + 1;
 			sscanf(ptr, "%ld", &temp);
 			used += temp;
 			continue;
 		}
+
 		if (strstr(buffer, "MemFree") != 0) {
 			ptr = (strchr(buffer, ':')) + 1;
 			sscanf(ptr, "%ld", &temp);
 			used -= temp;
 			continue;
 		}
+
 		if (strstr(buffer, "Buffers") != 0) {
 			ptr = (strchr(buffer, ':')) + 1;
 			sscanf(ptr, "%ld", &temp);
 			used -= temp;
 			continue;
 		}
+
 		if (strstr(buffer, "Cached") != 0) {
 			ptr = (strchr(buffer, ':')) + 1;
 			sscanf(ptr, "%ld", &temp);
@@ -50,13 +55,18 @@ long calculateused() {
 }
 
 
-void executebutton() {
+void
+executebutton()
+{
 	char *env = getenv("BLOCK_BUTTON");
-	if (env != NULL && env[0] == '2') 
+
+	if (env && env[0] == '2') 
 		forkexecv((char*) htoppath, (char**)htopargs, "dwmblocks-memory");
 }
 
-int main(void) {
+int
+main(void)
+{
 	executebutton();
 	printf(CLR_3"   %.1lfGiB"NRM"\n", ((calculateused())/1024.0)/1024.0);
 	return 0;
