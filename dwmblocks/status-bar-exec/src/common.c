91faaa432116e2e5b40984e09031944383bb5933
Dimitris <dimgerasimou@gmail.com>
2024-05-18 22:20:12 +0300

    Added second newline at logs

--- dwmblocks/status-bar-exec/src/common.c
+++ dwmblocks/status-bar-exec/src/common.c
@@ -78,7 +78,7 @@ log_string(const char *string, const char *argv0)
 	time(&rawtime);
 	timeinfo = localtime(&rawtime);
 
-	fprintf(fp, "%d-%d-%d %d:%d:%d %s\n%s\n", timeinfo->tm_year+1900,
+	fprintf(fp, "%d-%d-%d %d:%d:%d %s\n%s\n\n", timeinfo->tm_year+1900,
 		timeinfo->tm_mon+1, timeinfo->tm_mday, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, argv0, string);
 	
 	if (fp)
