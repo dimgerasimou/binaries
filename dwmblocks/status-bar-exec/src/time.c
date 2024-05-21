16eddd44bdc7e026f2d3da11ae05beff52b886b7
Dimitris <dimgerasimou@gmail.com>
2024-05-21 04:19:20 +0300

    Unspaghetti time.c

--- dwmblocks/status-bar-exec/src/time.c
+++ dwmblocks/status-bar-exec/src/time.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <time.h>
+
 #include "../include/colorscheme.h"
 
 int
-main()
+main(void)
 {
-	time_t currentTime   = time(NULL);
-	struct tm* localTime = localtime(&currentTime);
+	time_t     currentTime   = time(NULL);
+	struct tm* localTime     = localtime(&currentTime);
 
 	printf(CLR_7"   %.2d:%.2d"NRM"\n", localTime->tm_hour, localTime->tm_min);
 
