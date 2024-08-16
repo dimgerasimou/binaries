d7f284f62b9258bbaee4d956bea37304e1cc784d
Dimitrios <dimgerasimou@gmail.com>
2024-08-16 17:27:01 +0300

    Added keyboard click language switch

--- dwmblocks/status-bar-exec/src/keyboard.c
+++ dwmblocks/status-bar-exec/src/keyboard.c
@@ -7,6 +7,9 @@
 #include "../include/colorscheme.h"
 #include "../include/common.h"
 
+const char *swpath[] = { "$HOME", ".local", "bin", "dwm", "keyboard.sh", NULL };
+const char *swargs[] = { "keyboard.sh", NULL };
+
 static void
 XkbRF_FreeVarDefs(XkbRF_VarDefsRec *var_defs)
 {
@@ -21,6 +24,28 @@ XkbRF_FreeVarDefs(XkbRF_VarDefsRec *var_defs)
 	free(var_defs->extra_values);
 }
 
+static void
+execbuton(void)
+{
+	char *env;
+	char *path;
+
+	if (!(env = getenv("BLOCK_BUTTON")))
+		return;
+
+	switch(atoi(env)) {
+	case 1:
+		path = get_path((char**) swpath, 1);
+		unsetenv("BLOCK_BUTTON");
+		forkexecv(path, (char**) swargs, "dwmblocks-keyboard");
+		free(path);
+		break;
+
+	default:
+		break;
+	}
+}
+
 int
 main(void)
 {
@@ -29,10 +54,10 @@ main(void)
 	XkbRF_VarDefsRec vd;
 	char             *tok;
 
-	if (!(dpy = XOpenDisplay(NULL))) {
-		log_string("Cannot open X11 display", "dwmblocks-keyboard");
-		return errno;
-	}
+	execbuton();
+
+	if (!(dpy = XOpenDisplay(NULL)))
+		logwrite("XOpenDisplay() failed", NULL, LOG_ERROR, "dwmblocks-keyboard");
 
 	XkbGetState(dpy, XkbUseCoreKbd, &state);
 	XkbRF_GetNamesProp(dpy, NULL, &vd);
@@ -41,10 +66,8 @@ main(void)
 
 	for (int i = 0; i < state.group; i++) {
 		tok = strtok(NULL, ",");
-		if (!tok) {
-			log_string("Invalid layout for given group", "dwmblocks-keyboard");
-			return EXIT_FAILURE;
-		}
+		if (!tok)
+			logwrite("Invalid layout for given group", NULL, LOG_ERROR, "dwmblocks-keyboard");
 	}
 
 	printf(CLR_5"   %s"NRM"\n", tok);
